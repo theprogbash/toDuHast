e2e21e66e5f8648c5d45d9ead20024f99acd2f7d
Kamran Mmmdv <amorfrap@gmail.com>
2020-04-04 16:21:37 +0400

    Added task-update url

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('task-list/', views.taskList, name="task-list"),
     path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
-    path('task-create/', views.taskCreate, name='task-create')
+    path('task-create/', views.taskCreate, name='task-create'),
+    path('task-update/<str:pk>', views.taskCreate, name='task-update'),
 ]
