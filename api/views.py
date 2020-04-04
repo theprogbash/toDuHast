bfdd10f43cd054911237aa0c2a5ca81578851678
Kamran Mmmdv <amorfrap@gmail.com>
2020-04-04 16:23:36 +0400

    Created taskDelete view

--- api/views.py
+++ api/views.py
@@ -47,3 +47,10 @@ def taskUpdate(request, pk):
         serializer.save()
         
     return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+        
+    return Response(serializer.data)
