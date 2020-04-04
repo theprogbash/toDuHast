d02d3034277bdeabfad0a584b9fcc1bbf7836aa4
Kamran Mmmdv <amorfrap@gmail.com>
2020-04-04 16:24:58 +0400

    Fixed mistake in task-delete url

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
     path('task-create/', views.taskCreate, name='task-create'),
     path('task-update/<str:pk>', views.taskCreate, name='task-update'),
-    path('task-delete/<str:pk>', views.taskCreate, name='task-delete'),
+    path('task-delete/<str:pk>', views.taskDelete, name='task-delete'),
 ]
